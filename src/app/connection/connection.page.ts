82efb8f8007be71c5aa05fb5a4c1139c51516e19
Dardanboy <dardan.iljazi@gmail.com>
2019-05-26 23:14:55 +0200

    Add link for fingerprint installation

--- src/app/connection/connection.page.ts
+++ src/app/connection/connection.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FingerprintAIOOriginal, FingerprintAIO, FingerprintOptions } from '@ionic-native/fingerprint-aio';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
 
 @Component({
   selector: 'app-connection',
@@ -8,16 +8,14 @@ import { FingerprintAIOOriginal, FingerprintAIO, FingerprintOptions } from '@ion
 })
 export class ConnectionPage implements OnInit {
 
-  constructor(private faio: FingerprintAIOOriginal) {
-    this.faio.show({
-      clientId: 'Fingerprint-Demo', //Android: Used for encryption. iOS: used for dialogue if no `localizedReason` is given.
-      clientSecret: 'o7aoOMYUbyxaD23oFAnJ', //Necessary for Android encrpytion of keys. Use random secret key.
-      disableBackup:true,  //Only for Android(optional)
-      localizedFallbackTitle: 'Use Pin', //Only for iOS
-      localizedReason: 'Please authenticate' //Only for iOS
-    })
-        .then((result: any) => console.log(result))
-        .catch((error: any) => console.log(error));
+  constructor(private faio: FingerprintAIO) {
+
+      faio.isAvailable().then( (result) => {
+          alert("Fingerprint available");
+
+      }).catch((onmessage) => {
+          alert(onmessage);
+      });
   }
 
   ngOnInit() {
